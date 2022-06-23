976920fc9fd91e97e0b83f0099a742ab3568577d
Artur Correia <76447242+afarturc@users.noreply.github.com>
2022-06-23 03:19:00 +0100

    Changes

--- Forms/ResearchBase/ResearchBase/Main.Designer.vb
+++ Forms/ResearchBase/ResearchBase/Main.Designer.vb
@@ -79,9 +79,9 @@ Partial Class Main
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(134, 52)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(974, 91)
+        Me.Label1.Size = New System.Drawing.Size(1001, 91)
         Me.Label1.TabIndex = 4
-        Me.Label1.Text = "Welcome to ReseachBase"
+        Me.Label1.Text = "Welcome to ResearchBase"
         '
         'Main
         '
